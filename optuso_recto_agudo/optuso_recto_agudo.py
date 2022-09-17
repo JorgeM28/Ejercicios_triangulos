269b4fd2403a7a3468480ce462270e89765ec63c
JorgeM28 <jorge2224764@correo.uis.edu.co>
2022-09-17 07:20:42 -0500

    se actualizo el procesing

--- optuso_recto_agudo/optuso_recto_agudo.py
+++ optuso_recto_agudo/optuso_recto_agudo.py
@@ -6,7 +6,7 @@ print("--------------------------------------------")
 
 
 #input
-
+print("Ingresa tus datos donde c es el lado mayor")
 A=int(input("\nIngrese el dato del lado a: "))
 B=int(input("Ingrese el dato del lado b: "))
 C=int(input("Ingrese el dato del lado c: "))
@@ -15,11 +15,11 @@ C=int(input("Ingrese el dato del lado c: "))
 
 if (A+B)>C and (A+C)>B and (B+C)>A:
 
-    if C^2 < A^2 + B^2:
+    if C**2 < A**2 + B**2:
         print("Los datos ingresados forman un triangulo agudo")
-    elif C^2 == A^2 + B^2:
+    elif C**2 == A**2 + B**2:
         print("Los datos ingresados forman un triangulo recto")
-    elif C^2 > A^2 + B^2:
+    elif C**2 > A**2 + B**2:
         print("Los datos ingresados forman un triangulo obtuso")
     else:
         print("Los datos ingresados no forman ninguno de los 3 tipos de triangulo")
